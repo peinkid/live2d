ec4f5fa4bf35143df22907a8923c8e865828accf
peinkid <1611851246@qq.com>
2019-09-30 17:02:34 +0800

    blog

--- static/live2d/js/message.js
+++ static/live2d/js/message.js
@@ -39,12 +39,15 @@ function initTips() {
     success: function (result) {
       $.each(result.mouseover, function (index, tips) {
         $(tips.selector).mouseover(function () {
+          // if (tips.selector == '.gallery-section img') {
+          //   $('#landlord').hide()
+          // }
           var text = tips.text;
           if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
           text = text.renderTip({
             text: $(this).text()
           });
-          showMessage(text, 3000);
+          showMessage(text, 1000);
         });
       });
       $.each(result.click, function (index, tips) {
